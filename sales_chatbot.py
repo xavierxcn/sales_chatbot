cb3dc99911d869bf55eb4200aab6065b6687fb53
xavier <xavier@xavierx.cn>
2024-04-13 13:27:46 +0800

    Initial commit

---
+++ sales_chatbot.py
@@ -0,0 +1,70 @@
+import argparse
+
+import gradio as gr
+
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain.chains import RetrievalQA
+from langchain_openai import ChatOpenAI
+
+
+parser = argparse.ArgumentParser(description='销售大全')
+parser.add_argument('--bot_type', type=str,
+                    help='销售机器人类型', default="real_estates_sale")
+
+args = parser.parse_args()
+
+REAL_ESTATE_SALES_BOT = None
+LLM_CASE_SALES_BOT = None
+
+def initialize_sales_bot(vector_store_dir: str="real_estates_sale"):
+    db = FAISS.load_local(vector_store_dir, OpenAIEmbeddings(), allow_dangerous_deserialization=True)
+    llm = ChatOpenAI(model_name="gpt-4", temperature=0)
+    
+    bot = RetrievalQA.from_chain_type(llm,
+                                           retriever=db.as_retriever(search_type="similarity_score_threshold",
+                                                                     search_kwargs={"score_threshold": 0.5}))
+    # 返回向量数据库的检索结果
+    bot.return_source_documents = True
+
+    return bot
+
+def sales_chat(message, history):
+    print(f"[message]{message}")
+    print(f"[history]{history}")
+    # TODO: 从命令行参数中获取
+    bot_type = args.bot_type
+    enable_chat = True
+
+    bot = REAL_ESTATE_SALES_BOT if bot_type == "real_estates_sale" else LLM_CASE_SALES_BOT
+    ans = bot({"query": message})
+
+    # 如果检索出结果，或者开了大模型聊天模式
+    # 返回 RetrievalQA combine_documents_chain 整合的结果
+    if ans["source_documents"] or enable_chat:
+        print(f"[result]{ans['result']}")
+        print(f"[source_documents]{ans['source_documents']}")
+        return ans["result"]
+    # 否则输出套路话术
+    else:
+        return "这个问题我要问问领导"
+    
+
+def launch_gradio():
+    demo = gr.ChatInterface(
+        fn=sales_chat,
+        title="销售大全",
+        # retry_btn=None,
+        # undo_btn=None,
+        chatbot=gr.Chatbot(height=600),
+    )
+
+    demo.launch(share=True, server_name="0.0.0.0")
+
+if __name__ == "__main__":
+    # 初始化房产销售机器人
+    REAL_ESTATE_SALES_BOT = initialize_sales_bot("real_estates_sale")
+    LLM_CASE_SALES_BOT = initialize_sales_bot("llm_case_sale")
+
+    # 启动 Gradio 服务
+    launch_gradio()
